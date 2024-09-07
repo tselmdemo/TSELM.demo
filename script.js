abdf71301084c1b950c116293d3fa5a5c7d9c87c
Beilong Tang <bt132@duke.edu>
2024-09-07 20:30:36 +0800

    update

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function addRows() {
 
     const audio_path = "assets/demo"
 
-    const order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    const order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     
     // Number of rows to add
-    const numberOfRows = 8;
+    const numberOfRows = 12;
     
     // Number of audio elements per row
     const audiosPerRow = 7;
