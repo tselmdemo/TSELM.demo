a7e23981a9c259db932d6fe9baf9c7df41d8200d
Beilong Tang <bt132@duke.edu>
2024-09-10 20:29:46 +0800

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addRows(name, order) {
     const numberOfRows = order.length;
     
     // Number of audio elements per row
-    const audiosPerRow = len(audioSources);
+    const audiosPerRow = audioSources.length;
 
     // Add rows
     for (let i = 0; i < numberOfRows; i++) {
